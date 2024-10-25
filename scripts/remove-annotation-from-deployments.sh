c2598644e21568d4a81ec909237a8b155f2175d2
Jonathan Sabo <jonathan@sabo.io>
2024-10-25 15:46:38 -0400

    wip

---
+++ scripts/remove-annotation-from-deployments.sh
@@ -0,0 +1,69 @@
+kubectl annotate deployment otel-demo-accountingservice \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-adservice \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-cartservice \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-checkoutservice \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-currencyservice \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-emailservice \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-flagd \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-frauddetectionservice \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-frontend \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-frontendproxy \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-grafana \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-imageprovider \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-jaeger \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-kafka \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-loadgenerator \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-otelcol \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-paymentservice \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-productcatalogservice \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-prometheus-server \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-quoteservice \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-recommendationservice \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-shippingservice \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
+kubectl annotate deployment otel-demo-valkey \
+  gremlin.com/service-id- --overwrite -n otel-demo
+
